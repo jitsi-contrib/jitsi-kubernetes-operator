b777296b6b4aed2a4b34cf0dcf0c089e673b98e1
Hugo Renard <hugo.renard@protonmail.com>
2021-08-03 17:54:43 +0200

    missed

--- controllers/jibri.go
+++ controllers/jibri.go
@@ -115,10 +115,6 @@ func NewJibriDeploymentSyncer(jitsi *v1alpha1.Jitsi, c client.Client) syncer.Int
 						},
 					},
 				},
-				{
-					Name:  "DISPLAY",
-					Value: "0",
-				},
 			},
 			VolumeMounts: []corev1.VolumeMount{
 				{
@@ -149,21 +145,7 @@ func NewJibriDeploymentSyncer(jitsi *v1alpha1.Jitsi, c client.Client) syncer.Int
 
 		dep.Spec.Template.Spec.Containers = []corev1.Container{jibriContainer, jibriExporterContainer}
 
-		dep.Spec.Template.Spec.Affinity = &corev1.Affinity{
-			PodAntiAffinity: &corev1.PodAntiAffinity{
-				PreferredDuringSchedulingIgnoredDuringExecution: []corev1.WeightedPodAffinityTerm{
-					{
-						Weight: 100,
-						PodAffinityTerm: corev1.PodAffinityTerm{
-							LabelSelector: &metav1.LabelSelector{
-								MatchLabels: jitsi.ComponentLabels("jvb"),
-							},
-							TopologyKey: "kubernetes.io/hostname",
-						},
-					},
-				},
-			},
-		}
+		injectJibriAffinity(jitsi, &dep.Spec.Template.Spec)
 
 		return nil
 	})
